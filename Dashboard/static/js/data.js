51d9f10d4eabc0867ca9312f758a1840831436ae
Alaina Sliwinski <asliwinski23@gmail.com>
2021-07-22 21:07:34 -0400

    updated website

--- Dashboard/static/js/data.js
+++ Dashboard/static/js/data.js
@@ -1,31 +1,41 @@
 var data = [
   {
     model_type: "Random Forest",
-    features: "165",
-    r_squared_train: ".45",
-    r_squared_test: ".28"
+    features: "234",
+    r_squared_train: ".18",
+    r_squared_test: ".16"
   },
   {
     model_type: "Random Forest",
     features: "120",
-    r_squared_train: ".45",
-    r_squared_test: ".28"
+    r_squared_train: ".18",
+    r_squared_test: ".16"
   },
   {
     model_type: "Random Forest",
     features: "41",
-    r_squared_train: ".45",
-    r_squared_test: ".28"
+    r_squared_train: ".18",
+    r_squared_test: ".16"
   },
   {
     model_type: "Random Forest",
     features: "1",
-    r_squared_train: ".45",
-    r_squared_test: ".28"
+    r_squared_train: ".18",
+    r_squared_test: ".16"
+  },
+  {
+    model_type: "Multiple Regression",
+    features: "234",
+    r_squared_test: ".14"
+  },
+  {
+    model_type: "Multiple Regression",
+    features: "120",
+    r_squared_test: ".14"
   },
   {
     model_type: "Multiple Regression",
     features: "41",
-    r_squared_test: ".05",
+    r_squared_test: ".16"
   },
 ];
